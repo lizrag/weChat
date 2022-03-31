ba3a5140b96ea22f3cfd82f8bb0f824e2f16e9ff
Oscar Carvajal <os.carvajalmora@gmail.com>
2022-03-31 18:23:40 -0300

    Add styles and custom functions

--- public/index.js
+++ public/index.js
@@ -1,32 +1,48 @@
 let socket = io();
 let chatBox = document.getElementById('chatBox');
 let log = document.getElementById('log');
+let title = document.getElementById('title');
 let user;
 
 Swal.fire({
     title:"Identify yourself",
     input:'text',
     allowOutsideClick:false,
-    inputValidator: (value) =>{
-        return !value && 'Please write your user name'
+    inputValidator: (value) => {
+        return !value && 'Please write your user name';
     }
-}).then(result=>{
+}).then(result => {
     user = result.value;
+    localStorage.setItem('username', user);
+    title.innerHTML = `Hello, <span>${user}</span>! welcome to weChat!`
 })
 
-chatBox.addEventListener('keyup',evt=>{
-    if(evt.key==="Enter"){
-        if(chatBox.value.trim().length>0){//Por lo menos se envía un símbolo
-            socket.emit('message',{user,message:chatBox.value.trim()})
+chatBox.addEventListener('keyup', evt => {
+    if(evt.key === "Enter"){
+        if(chatBox.value.trim().length > 0){ //Por lo menos se envía un símbolo
+            const date = new Date();
+            const currentTime = `${date.getHours()}:${date.getMinutes()}`;
+            socket.emit('message', {
+                user,
+                message:chatBox.value.trim(),
+                currentTime 
+            });
             chatBox.value="";
         }
     }
 })
+
 /*SOCKETS */
-socket.on('log',data=>{
-    let messages="";
-    data.forEach(log=>{
-        messages= messages + `${log.user} dice: ${log.message}</br>`
+socket.on('log', data => {
+    let messages = "";
+    data.forEach(log => {
+        if(log.user === localStorage.getItem('username')) {
+            messages = messages + `<div class="message message-me">${log.message} <span>${log.currentTime}</span></div>`
+        } else {
+            messages = messages + `<div class="message message-other">${log.user} says: ${log.message} <span>${log.currentTime}</span> </div>`
+        }
+        
     })
-    log.innerHTML=messages;
+    log.innerHTML = messages;
+    log.scrollTop = log.scrollHeight; // Estudiar
 })
--- src/app.js
+++ src/app.js
@@ -4,10 +4,13 @@ import __dirname from './utils.js';
 
 const app = express();
 const PORT = process.env.PORT||8080;
-app.use(express.static(__dirname+'/public'))
-const server = app.listen(PORT,()=>{
-    console.log(`Listening on PORT ${PORT}`)
-})
+
+app.use(express.static('public'));
+
+const server = app.listen(PORT, () => {
+    console.log(`Listening on PORT ${PORT}`);
+});
+
 const io = new Server(server);
 
 const log = [];
@@ -15,5 +18,5 @@ io.on('connection',socket=>{
     socket.on('message',data=>{
         log.push(data);
         io.emit('log',log)
-    })
-})
+    });
+});
